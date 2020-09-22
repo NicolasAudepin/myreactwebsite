76be0bb421f060d29802e429f25ce7dccd5b7387
NicolasAudepin <42012602+NicolasAudepin@users.noreply.github.com>
2020-09-23 01:19:00 +0200

    Updates

---
+++ precache-manifest.96b8dc5c6029188b43f4a239a6429ae4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98e88ad9eb7d35a7960ac9dd12a95a8e",
+    "url": "/myreactwebsite/index.html"
+  },
+  {
+    "revision": "47ab3360f1cdb50456f3",
+    "url": "/myreactwebsite/static/css/main.6598bcba.chunk.css"
+  },
+  {
+    "revision": "55a3b5bbde924d4fd82e",
+    "url": "/myreactwebsite/static/js/2.ef7594aa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myreactwebsite/static/js/2.ef7594aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47ab3360f1cdb50456f3",
+    "url": "/myreactwebsite/static/js/main.82c6130c.chunk.js"
+  },
+  {
+    "revision": "8887e9d04315e9260d34",
+    "url": "/myreactwebsite/static/js/runtime-main.e15783db.js"
+  }
+]);
