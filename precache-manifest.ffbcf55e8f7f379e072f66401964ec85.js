57952fe9bf7bfc410c74f8b0c88c470525237a27
NicolasAudepin <nicolas.audepin@gmail.com>
2020-09-28 22:50:48 +0200

    Updates

---
+++ precache-manifest.ffbcf55e8f7f379e072f66401964ec85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f2c5020ce1daa180cdc53e73c4f8db6",
+    "url": "/myreactwebsite/index.html"
+  },
+  {
+    "revision": "cf018b8046bf0affd836",
+    "url": "/myreactwebsite/static/css/main.d760c1c4.chunk.css"
+  },
+  {
+    "revision": "4e724af02bc4cf86a41e",
+    "url": "/myreactwebsite/static/js/2.16e51a20.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myreactwebsite/static/js/2.16e51a20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf018b8046bf0affd836",
+    "url": "/myreactwebsite/static/js/main.7e9101ff.chunk.js"
+  },
+  {
+    "revision": "8887e9d04315e9260d34",
+    "url": "/myreactwebsite/static/js/runtime-main.e15783db.js"
+  }
+]);
